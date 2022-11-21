10dccc4a8b818943d0f7c760331638c62a54a7cc
Olayinka Shanu <ademayowa4@gmail.com>
2022-11-21 08:46:46 +0800

    Card project

---
+++ index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form");
+const errors = document.querySelector(".error");
+const notBlank = document.querySelector(".no-blanks");
+const name = document.getElementById("name");
+const number = document.getElementById("number");
+const cvc = document.getElementById("cvc");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const numbersOnly = document.getElementById("numbers-only");
+
